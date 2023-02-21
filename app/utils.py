bddbefe7b5856308eea96869dcaeac0f9e7b570c
And <ase@crimea.com>
2023-02-21 11:19:55 +0300

    add func to make base64 file

--- app/utils.py
+++ app/utils.py
@@ -64,5 +64,15 @@ def get_qr_code_config():
     buffer = io.BytesIO()
     img.save(buffer, format="PNG")
 
-    base_64_mage = base64.b64encode(buffer.getvalue()).decode("utf-8")
-    return base_64_mage
+    base_64_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    return base_64_image
+
+
+def make_base64_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = file.read()
+            base64file = base64.b64encode(data.encode('utf-8')).decode('utf-8')
+            return base64file
+    else:
+        raise FileNotFoundError(f'file {file_path} not found')
