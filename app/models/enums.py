c68e6338e68eb70a822b6c5a9e8ea26ca2c1ca82
And <ase@crimea.com>
2023-02-22 18:18:48 +0300

    add container elements

--- app/models/enums.py
+++ app/models/enums.py
@@ -3,47 +3,6 @@ from typing import Literal
 from enum import Enum
 
 
-class ElementType(Enum):
-    linear_layout = Literal['LinearLayout']
-    barcode = Literal['barcode']
-    horizontal_gallery = Literal['HorizontalGallery']
-    voice = Literal['voice']
-    photo = Literal['photo']
-    photo_gallery = Literal['photoGallery']
-    signature = Literal['signature']
-    vision = Literal['Vision']
-    cart = Literal['Cart']
-    tiles = Literal['Tiles']
-    image_slider = Literal['ImageSlider']
-    menu_item = Literal['MenuItem']
-
-
-class ContainerElementType(str, Enum):
-    text_view: str = 'TextView'
-    button: str = 'Button'
-    edit_text_text: str = 'EditTextText'
-    edit_text_numeric: str = 'EditTextNumeric'
-    edit_text_pass: str = 'EditTextPass'
-    edit_text_auto: str = 'EditTextAuto'
-    edit_text_autocomplete: str = 'EditTextAutocomplete'
-    modern_edit_text: str = 'ModernEditText'
-    picture: str = 'Picture'
-    check_box: str = 'CheckBox'
-    gauge: str = 'Gauge'
-    chart: str = 'Chart'
-    spinner_layout: str = 'SpinnerLayout'
-    table_layout: str = 'TableLayout'
-    cart: str = 'Cart'
-    multiline_text: str = 'MultilineText'
-    cards_layout: str = 'CardsLayout'
-    c_buttons: str = 'CButtons'
-    c_buttons_horizontal: str = 'CButtonsHorizontal'
-    date_field: str = 'DateField'
-    progress_button: str = 'ProgressButton'
-    html: str = 'html'
-    map: str = 'map'
-
-
 class ElementsIcon(str, Enum):
     forward: str = 'forward'
     backward: str = 'backward'
