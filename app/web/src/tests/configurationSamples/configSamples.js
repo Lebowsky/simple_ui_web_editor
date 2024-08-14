b77e2c32e8c87e6a43e24dadedabe02b328fc8b0
And <ase@crimea.com>
2024-08-14 20:47:58 +0300

    add samples

---
+++ app/web/src/tests/configurationSamples/configSamples.js
@@ -0,0 +1,79 @@
+export const emptyConfig = {
+
+}
+
+export const rootConfig = {
+  "ClientConfiguration": {
+    "ConfigurationName": "Новая конфигурация",
+    "ConfigurationDescription": "",
+    "ConfigurationVersion": "0.0.1",
+    "ConfigurationTags": "",
+    "RunPython": true,
+  }
+}
+
+export const configurationWithoutProcesses = {
+  "ClientConfiguration": {
+    "ConfigurationName": "Новая конфигурация",
+    "ConfigurationDescription": "",
+    "ConfigurationVersion": "0.0.1",
+    "ConfigurationSettings": {
+      "uid": "dd7ae0a1209a40269d1061892508c8f1"
+    },
+    "ConfigurationTags": "",
+    "RunPython": true,
+    "MainMenu": [],
+    "Mediafile": [],
+    "pyHandlersPath": "",
+    "PyTimerTask": [],
+    "PyFiles": [],
+    "StyleTemplates": [],
+    "arch2": true,
+    "CommonHandlers": []
+  }
+}
+
+export const configurationWithOneProcessAndOneOperation = {
+  "ClientConfiguration": {
+    "ConfigurationName": "Новая конфигурация",
+    "ConfigurationDescription": "",
+    "ConfigurationVersion": "0.0.1",
+    "Processes": [
+      {
+        "type": "Process",
+        "ProcessName": "Новый процесс",
+        "DefineOnBackPressed": false,
+        "hidden": false,
+        "login_screen": false,
+        "SC": false,
+        "Operations": [
+          {
+            "type": "Operation",
+            "Name": "Новый экран",
+            "Timer": false,
+            "hideToolBarScreen": false,
+            "hideBottomBarScreen": false,
+            "noScroll": false,
+            "handleKeyUp": false,
+            "noConfirmation": false,
+            "Elements": [],
+            "Handlers": []
+          }
+        ]
+      },
+    ],
+    "ConfigurationSettings": {
+      "uid": "dd7ae0a1209a40269d1061892508c8f1"
+    },
+    "ConfigurationTags": "",
+    "RunPython": true,
+    "MainMenu": [],
+    "Mediafile": [],
+    "pyHandlersPath": "",
+    "PyTimerTask": [],
+    "PyFiles": [],
+    "StyleTemplates": [],
+    "arch2": true,
+    "CommonHandlers": []
+  }
+}
